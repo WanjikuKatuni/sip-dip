63fbd2044793dfe589f9ce64c7245ab00465ff19
wanjiku <annritawanjiku77@gmail.com>
2022-09-01 18:03:05 +0300

    added loop that gets ingredients and measure

--- app/js/index.js
+++ app/js/index.js
@@ -14,8 +14,34 @@ if (userInput.length == 0){
     alert `The search input field cannot be empty`;
 }
 else{
+    // console.log(`${url}${userInput}`)
     fetch(`${url}${userInput}`)
 
-    .then((res => res.json))
-    .then((data)=> console.log(data))
+    .then((res) => res.json())
+    .then((data)=> {
+        console.log(data)
+        console.log(data.drinks[0])
+        let myDrink = data.drinks[0];
+        console.log(myDrink.strDrink)
+        // console.log(myDrink.strDrinkThumb)
+        // console.log(myDrink.strIngredient1)
+
+        let count = 1
+        let ingredients = []
+        for (let i in myDrink){
+            let ingredient="";
+            let measure =""
+            if (i.startsWith("strIngredient") && myDrink[i]){
+                ingredient = myDrink[i];
+                if (myDrink[`strMeasure${count}`]){
+                    measure = myDrink[`strMeasure${count}`]
+                }
+                else {
+                    measure=  ""
+                }
+                count += 1;
+                ingredients.push(`${measure} ${ingredient}` )
+            }
+        }
+    });
 }
